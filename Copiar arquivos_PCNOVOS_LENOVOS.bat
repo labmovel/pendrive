9bf1f448c04f7aa1b30b09d41a0f5ce7049b7dde
labmovel <fabriciofalerios1987@gmail.com>
2026-02-07 17:05:15 -0300

    Add files via upload

--- Copiar arquivos_PCNOVOS_LENOVOS.bat
+++ Copiar arquivos_PCNOVOS_LENOVOS.bat
@@ -1,9 +1,9 @@
 @echo off
 cls
 
-xcopy C:\Users\lab.saoluis\Downloads\pendrive\cloud1\*.*  C:\Users\Public\Desktop\*.*
+xcopy C:\Users\lab.saoluis\Downloads\pendrive-main\pendrive-main\cloud1\*.*  C:\Users\Public\Desktop\*.*
 
-xcopy C:\Users\lab.saoluis\Downloads\pendrive\cloud1\*.*  C:\intel\*.*
+xcopy C:\Users\lab.saoluis\Downloads\pendrive-main\pendrive-main\intel\*.*  C:\intel\*.*
 
 RD /S /Q C:\RECYCLER\
 
